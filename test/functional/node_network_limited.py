tmagik/catoshi

[tests] refactor node_network_limited

test/functional/node_network_limited.py
@@ -2,6 +2,12 @@
 # Copyright (c) 2017 The Bitcoin Core developers
 # Distributed under the MIT software license, see the accompanying
 # file COPYING or http://www.opensource.org/licenses/mit-license.php.
+"""Tests NODE_NETWORK_LIMITED.
+
+Tests that a node configured with -prune=550 signals NODE_NETWORK_LIMITED correctly
+and that it responds to getdata requests for blocks correctly:
+    - send a block within 288 + 2 of the tip
+    - disconnect peers who request blocks older than that."""
 from test_framework.messages import CInv, msg_getdata
 from test_framework.mininode import NODE_BLOOM, NODE_NETWORK_LIMITED, NODE_WITNESS, NetworkThread, P2PInterface
 from test_framework.test_framework import BitcoinTestFramework
@@ -12,52 +18,40 @@ class P2PIgnoreInv(P2PInterface):
         # The node will send us invs for other blocks. Ignore them.
         pass
 
+    def send_getdata_for_block(self, blockhash):
+        getdata_request = msg_getdata()
+        getdata_request.inv.append(CInv(2, int(blockhash, 16)))
+        self.send_message(getdata_request)
+
 class NodeNetworkLimitedTest(BitcoinTestFramework):
     def set_test_params(self):
         self.setup_clean_chain = True
         self.num_nodes = 1
         self.extra_args = [['-prune=550']]
 
-    def get_signalled_service_flags(self):
-        node = self.nodes[0].add_p2p_connection(P2PIgnoreInv())
-        NetworkThread().start()
-        node.wait_for_verack()
-        services = node.nServices
-        self.nodes[0].disconnect_p2ps()
-        node.wait_for_disconnect()
-        return services
-
-    def try_get_block_via_getdata(self, blockhash, must_disconnect):
-        node = self.nodes[0].add_p2p_connection(P2PIgnoreInv())
-        NetworkThread().start()
-        node.wait_for_verack()
-        getdata_request = msg_getdata()
-        getdata_request.inv.append(CInv(2, int(blockhash, 16)))
-        node.send_message(getdata_request)
-
-        if (must_disconnect):
-            # Ensure we get disconnected
-            node.wait_for_disconnect(5)
-        else:
-            # check if the peer sends us the requested block
-            node.wait_for_block(int(blockhash, 16), 3)
-            self.nodes[0].disconnect_p2ps()
-            node.wait_for_disconnect()
-
     def run_test(self):
-        # NODE_BLOOM & NODE_WITNESS & NODE_NETWORK_LIMITED must now be signaled
-        assert_equal(self.get_signalled_service_flags(), NODE_BLOOM | NODE_WITNESS | NODE_NETWORK_LIMITED)
+        node = self.nodes[0].add_p2p_connection(P2PIgnoreInv())
+        NetworkThread().start()
+        node.wait_for_verack()
 
-        # Test the RPC service flags
-        assert_equal(int(self.nodes[0].getnetworkinfo()['localservices'], 16), NODE_BLOOM | NODE_WITNESS | NODE_NETWORK_LIMITED)
+        expected_services = NODE_BLOOM | NODE_WITNESS | NODE_NETWORK_LIMITED
 
-        # Now mine some blocks over the NODE_NETWORK_LIMITED + 2(racy buffer ext.) target
+        self.log.info("Check that node has signalled expected services.")
+        assert_equal(node.nServices, expected_services)
+
+        self.log.info("Check that the localservices is as expected.")
+        assert_equal(int(self.nodes[0].getnetworkinfo()['localservices'], 16), expected_services)
+
+        self.log.info("Mine enough blocks to reach the NODE_NETWORK_LIMITED range.")
         blocks = self.nodes[0].generate(292)
 
-        # Make sure we can max retrive block at tip-288
-        # requesting block at height 2 (tip-289) must fail (ignored)
-        self.try_get_block_via_getdata(blocks[1], False)  # last block in valid range
-        self.try_get_block_via_getdata(blocks[0], True)  # first block outside of the 288+2 limit
+        self.log.info("Make sure we can max retrive block at tip-288.")
+        node.send_getdata_for_block(blocks[1])  # last block in valid range
+        node.wait_for_block(int(blocks[1], 16), timeout=3)
+
+        self.log.info("Requesting block at height 2 (tip-289) must fail (ignored).")
+        node.send_getdata_for_block(blocks[0])  # first block outside of the 288+2 limit
+        node.wait_for_disconnect(5)
 
 if __name__ == '__main__':
     NodeNetworkLimitedTest().main()

bitfinitechain/bitfinite-core

Summary:
Partial backport of core PR11867 (commit ee5efad)
Depends on D2369
Test Plan:
  ./test/functional/test_runner.py node_network_limited.py
Reviewers: #bitcoin_abc, deadalnix
Reviewed By: #bitcoin_abc, deadalnix
Subscribers: teamcity, schancel
Differential Revision: https://reviews.bitcoinabc.org/D2370

test/functional/node_network_limited.py
@@ -2,6 +2,12 @@
 # Copyright (c) 2017 The Bitcoin Core developers
 # Distributed under the MIT software license, see the accompanying
 # file COPYING or http://www.opensource.org/licenses/mit-license.php.
+"""Tests NODE_NETWORK_LIMITED.
+
+Tests that a node configured with -prune=550 signals NODE_NETWORK_LIMITED correctly
+and that it responds to getdata requests for blocks correctly:
+    - send a block within 288 + 2 of the tip
+    - disconnect peers who request blocks older than that."""
 from test_framework.messages import CInv, msg_getdata
 from test_framework.mininode import NODE_BLOOM, NODE_NETWORK_LIMITED, NODE_BITCOIN_CASH, NetworkThread, P2PInterface
 from test_framework.test_framework import BitcoinTestFramework
@@ -13,6 +19,11 @@ class P2PIgnoreInv(P2PInterface):
         # The node will send us invs for other blocks. Ignore them.
         pass
 
+    def send_getdata_for_block(self, blockhash):
+        getdata_request = msg_getdata()
+        getdata_request.inv.append(CInv(2, int(blockhash, 16)))
+        self.send_message(getdata_request)
+
 
 class NodeNetworkLimitedTest(BitcoinTestFramework):
     def set_test_params(self):
@@ -20,50 +31,34 @@ class NodeNetworkLimitedTest(BitcoinTestFramework):
         self.num_nodes = 1
         self.extra_args = [['-prune=550']]
 
-    def get_signalled_service_flags(self):
-        node = self.nodes[0].add_p2p_connection(P2PIgnoreInv())
-        NetworkThread().start()
-        node.wait_for_verack()
-        services = node.nServices
-        self.nodes[0].disconnect_p2ps()
-        node.wait_for_disconnect()
-        return services
-
-    def try_get_block_via_getdata(self, blockhash, must_disconnect):
-        node = self.nodes[0].add_p2p_connection(P2PIgnoreInv())
-        NetworkThread().start()
-        node.wait_for_verack()
-        getdata_request = msg_getdata()
-        getdata_request.inv.append(CInv(2, int(blockhash, 16)))
-        node.send_message(getdata_request)
-
-        if (must_disconnect):
-            # Ensure we get disconnected
-            node.wait_for_disconnect(5)
-        else:
-            # check if the peer sends us the requested block
-            node.wait_for_block(int(blockhash, 16), 3)
-            self.nodes[0].disconnect_p2ps()
-            node.wait_for_disconnect()
-
     def run_test(self):
-        # NODE_BLOOM & NODE_BITCOIN_CASH & NODE_NETWORK_LIMITED must now be signaled
-        assert_equal(self.get_signalled_service_flags(),
-                     NODE_BLOOM | NODE_BITCOIN_CASH | NODE_NETWORK_LIMITED)
+        node = self.nodes[0].add_p2p_connection(P2PIgnoreInv())
+        NetworkThread().start()
+        node.wait_for_verack()
 
-        # Test the RPC service flags
+        expected_services = NODE_BLOOM | NODE_BITCOIN_CASH | NODE_NETWORK_LIMITED
+
+        self.log.info("Check that node has signalled expected services.")
+        assert_equal(node.nServices, expected_services)
+
+        self.log.info("Check that the localservices is as expected.")
         assert_equal(int(self.nodes[0].getnetworkinfo()[
-                     'localservices'], 16), NODE_BLOOM | NODE_BITCOIN_CASH | NODE_NETWORK_LIMITED)
+                     'localservices'], 16), expected_services)
 
-        # Now mine some blocks over the NODE_NETWORK_LIMITED + 2(racy buffer ext.) target
+        self.log.info(
+            "Mine enough blocks to reach the NODE_NETWORK_LIMITED range.")
         blocks = self.nodes[0].generate(292)
 
-        # Make sure we can max retrive block at tip-288
-        # requesting block at height 2 (tip-289) must fail (ignored)
+        self.log.info("Make sure we can max retrive block at tip-288.")
         # last block in valid range
-        self.try_get_block_via_getdata(blocks[1], False)
+        node.send_getdata_for_block(blocks[1])
+        node.wait_for_block(int(blocks[1], 16), timeout=3)
+
+        self.log.info(
+            "Requesting block at height 2 (tip-289) must fail (ignored).")
         # first block outside of the 288+2 limit
-        self.try_get_block_via_getdata(blocks[0], True)
+        node.send_getdata_for_block(blocks[0])
+        node.wait_for_disconnect(5)
 
 
 if __name__ == '__main__':