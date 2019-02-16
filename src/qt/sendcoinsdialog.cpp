bitfinitechain/bitfinite-core

commit 0929e2007d236fb9d58a05758a72b66a058d53ad

remove minimum total fee option
Summary:
Backport of Core PR10390
https://github.com/bitcoin/bitcoin/pull/10390/files

Completes T522

Test Plan: bitcoin-qt removes the total radio button option as expected.

Reviewers: deadalnix, Fabien, #bitcoin_abc, Mengerian

Reviewed By: Fabien, #bitcoin_abc, Mengerian

Subscribers: markblundeberg, Mengerian, teamcity, schancel

Differential Revision: https://reviews.bitcoinabc.org/D2322

src/qt/sendcoinsdialog.cpp
@@ -158,7 +158,6 @@ SendCoinsDialog::SendCoinsDialog(const PlatformStyle *_platformStyle,
             std::max<int>(0, std::min(1, settings.value("nFeeRadio").toInt())))
         ->setChecked(true);
     ui->groupCustomFee->setId(ui->radioCustomPerKilobyte, 0);
-    ui->groupCustomFee->setId(ui->radioCustomAtLeast, 1);
     ui->groupCustomFee
         ->button(std::max<int>(
             0, std::min(1, settings.value("nCustomFeeRadio").toInt())))
@@ -742,10 +741,6 @@ void SendCoinsDialog::updateFeeSectionControls() {
     ui->radioCustomPerKilobyte->setEnabled(
         ui->radioCustomFee->isChecked() &&
         !ui->checkBoxMinimumFee->isChecked());
-    ui->radioCustomAtLeast->setEnabled(
-        ui->radioCustomFee->isChecked() &&
-        !ui->checkBoxMinimumFee->isChecked() &&
-        CoinControlDialog::coinControl->HasSelected());
     ui->customFee->setEnabled(ui->radioCustomFee->isChecked() &&
                               !ui->checkBoxMinimumFee->isChecked());
 }
@@ -753,19 +748,8 @@ void SendCoinsDialog::updateFeeSectionControls() {
 void SendCoinsDialog::updateGlobalFeeVariables() {
     if (ui->radioSmartFee->isChecked()) {
         payTxFee = CFeeRate(Amount::zero());
-
-        // set nMinimumTotalFee to 0 to not accidentally pay a custom fee
-        CoinControlDialog::coinControl->nMinimumTotalFee = Amount::zero();
     } else {
         payTxFee = CFeeRate(Amount(ui->customFee->value()));
-
-        // if user has selected to set a minimum absolute fee, pass the value to
-        // coincontrol
-        // set nMinimumTotalFee to 0 in case of user has selected that the fee
-        // is per KB
-        CoinControlDialog::coinControl->nMinimumTotalFee =
-            ui->radioCustomAtLeast->isChecked() ? ui->customFee->value()
-                                                : Amount::zero();
     }
 }
 
@@ -977,22 +961,6 @@ void SendCoinsDialog::coinControlChangeEdited(const QString &text) {
 // Coin Control: update labels
 void SendCoinsDialog::coinControlUpdateLabels() {
     if (!model || !model->getOptionsModel()) {
-        return;
-    }
-
-    if (model->getOptionsModel()->getCoinControlFeatures()) {
-        // enable minimum absolute fee UI controls
-        ui->radioCustomAtLeast->setVisible(true);
-
-        // only enable the feature if inputs are selected
-        ui->radioCustomAtLeast->setEnabled(
-            ui->radioCustomFee->isChecked() &&
-            !ui->checkBoxMinimumFee->isChecked() &&
-            CoinControlDialog::coinControl->HasSelected());
-    } else {
-        // in case coin control is disabled (=default), hide minimum absolute
-        // fee UI controls
-        ui->radioCustomAtLeast->setVisible(false);
         return;
     }
 

src/qt/coincontroldialog.cpp
@@ -547,10 +547,6 @@ void CoinControlDialog::updateLabels(WalletModel *model, QDialog *dialog) {
 
         // Fee
         nPayFee = GetMinimumFee(nBytes, nTxConfirmTarget, g_mempool);
-        if (nPayFee > Amount::zero() &&
-            coinControl->nMinimumTotalFee > nPayFee) {
-            nPayFee = coinControl->nMinimumTotalFee;
-        }
 
         if (nPayAmount > Amount::zero()) {
             nChange = nAmount - nPayAmount;
@@ -622,7 +618,7 @@ void CoinControlDialog::updateLabels(WalletModel *model, QDialog *dialog) {
     l7->setText(fDust ? tr("yes") : tr("no"));
     // Change
     l8->setText(BitcoinUnits::formatWithUnit(nDisplayUnit, nChange));
-    if (nPayFee > Amount::zero() && (coinControl->nMinimumTotalFee < nPayFee)) {
+    if (nPayFee > Amount::zero()) {
         l3->setText(ASYMP_UTF8 + l3->text());
         l4->setText(ASYMP_UTF8 + l4->text());
         if (nChange > Amount::zero() &&

src/qt/forms/sendcoinsdialog.ui
@@ -840,19 +840,6 @@
                   </widget>
                  </item>
                  <item>
-                  <widget class="QRadioButton" name="radioCustomAtLeast">
-                   <property name="toolTip">
-                    <string>If the custom fee is set to 1000 satoshis and the transaction is only 250 bytes, then &quot;per kilobyte&quot; only pays 250 satoshis in fee, while &quot;total at least&quot; pays 1000 satoshis. For transactions bigger than a kilobyte both pay by kilobyte.</string>
-                   </property>
-                   <property name="text">
-                    <string>total at least</string>
-                   </property>
-                   <attribute name="buttonGroup">
-                    <string notr="true">groupCustomFee</string>
-                   </attribute>
-                  </widget>
-                 </item>
-                 <item>
                   <widget class="BitcoinAmountField" name="customFee"/>
                  </item>
                  <item>

src/wallet/coincontrol.h
@@ -17,8 +17,6 @@ public:
     //! Includes watch only addresses which match the ISMINE_WATCH_SOLVABLE
     //! criteria
     bool fAllowWatchOnly;
-    //! Minimum absolute fee (not per kilobyte)
-    Amount nMinimumTotalFee;
     //! Override estimated feerate
     bool fOverrideFeeRate;
     //! Feerate to use if overrideFeeRate is true
@@ -33,7 +31,6 @@ public:
         fAllowOtherInputs = false;
         fAllowWatchOnly = false;
         setSelected.clear();
-        nMinimumTotalFee = Amount::zero();
         nFeeRate = CFeeRate(Amount::zero());
         fOverrideFeeRate = false;
         nConfirmTarget = 0;

src/wallet/wallet.cpp
@@ -3063,11 +3063,6 @@ bool CWallet::CreateTransaction(const std::vector<CRecipient> &vecSend,
 
             Amount nFeeNeeded =
                 GetMinimumFee(nBytes, currentConfirmationTarget, g_mempool);
-            if (coinControl && nFeeNeeded > Amount::zero() &&
-                coinControl->nMinimumTotalFee > nFeeNeeded) {
-                nFeeNeeded = coinControl->nMinimumTotalFee;
-            }
-
             if (coinControl && coinControl->fOverrideFeeRate) {
                 nFeeNeeded = coinControl->nFeeRate.GetFeeCeiling(nBytes);
             }

commit 3c7215a111c5993b9d7bb2a197bcde5f5dd3d180

fix Qt crash
Summary:
introduced in D2322 backport

Didn't affect most people, only those who had custom fee selected (which required using Coin Control)

Workaround for affected users: change nCustomFeeRadio=1 to nCustomFeeRadio=0 in the ~/.config/BitcoinABC/<whatever>.conf file.

same fix was also required in core: https://github.com/bitcoin/bitcoin/pull/11332

Test Plan: it runs now!

Reviewers: #bitcoin_abc, deadalnix, jasonbcox

Reviewed By: #bitcoin_abc, jasonbcox

Subscribers: teamcity, schancel

Differential Revision: https://reviews.bitcoinabc.org/D2559

src/qt/sendcoinsdialog.cpp
@@ -158,10 +158,7 @@ SendCoinsDialog::SendCoinsDialog(const PlatformStyle *_platformStyle,
             std::max<int>(0, std::min(1, settings.value("nFeeRadio").toInt())))
         ->setChecked(true);
     ui->groupCustomFee->setId(ui->radioCustomPerKilobyte, 0);
-    ui->groupCustomFee
-        ->button(std::max<int>(
-            0, std::min(1, settings.value("nCustomFeeRadio").toInt())))
-        ->setChecked(true);
+    ui->groupCustomFee->button(0)->setChecked(true);
     ui->customFee->setValue(
         int64_t(settings.value("nTransactionFee").toLongLong()) * SATOSHI);
     ui->checkBoxMinimumFee->setChecked(