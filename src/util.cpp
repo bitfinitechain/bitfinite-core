bitfinitechain/bitfinite-core

commit cde3f7ae01db84e40c1336fc6d07413851281c3c

Merge #13877: utils: Make fs::path::string() always return utf-8 string on Windows
Summary:
2c3eade704 Make fs::path::string() always return utf-8 string (Chun Kuan Lee)

Pull request description:

  Imbue `fs::path` with `std::codecvt_utf8_utf16` at `SetupEnvironment()`, so that default string encoding will be utf-8 inside `fs::path`.

Tree-SHA512: 0cb59464d777278decbf24771fc5ff0cb2caa7bc2fe8ee5cd36c97a2324873a3caad131f08f050393b488316ee7f4ab0b28b7fa4699e41839f8e51b9867d5118

Backport of Core PR13877
https://github.com/bitcoin/bitcoin/pull/13877/files

Test Plan:
```
make check
bitcoin-qt # sanity checks
```

Reviewers: #bitcoin_abc, deadalnix, Fabien

Reviewed By: #bitcoin_abc, Fabien

Differential Revision: https://reviews.bitcoinabc.org/D3241

src/util.cpp
@@ -1135,7 +1135,11 @@ void SetupEnvironment() {
     // A dummy locale is used to extract the internal default locale, used by
     // fs::path, which is then used to explicitly imbue the path.
     std::locale loc = fs::path::imbue(std::locale::classic());
+#ifndef WIN32
     fs::path::imbue(loc);
+#else
+    fs::path::imbue(std::locale(loc, new std::codecvt_utf8_utf16<wchar_t>()));
+#endif
 }
 
 bool SetupNetworking() {

src/fs.h
@@ -9,7 +9,6 @@
 #include <string>
 
 #include <boost/filesystem.hpp>
-#include <boost/filesystem/detail/utf8_codecvt_facet.hpp>
 #include <boost/filesystem/fstream.hpp>
 
 /** Filesystem operations and types */

src/qt/guiutil.cpp
@@ -41,7 +41,6 @@
 #include <shlwapi.h>
 #endif
 
-#include <boost/filesystem/detail/utf8_codecvt_facet.hpp>
 #include <boost/filesystem/fstream.hpp>
 #include <boost/scoped_array.hpp>
 
@@ -64,8 +63,6 @@
 #if QT_VERSION >= 0x50200
 #include <QFontDatabase>
 #endif
-
-static fs::detail::utf8_codecvt_facet utf8;
 
 #if defined(Q_OS_MAC)
 // These Mac includes must be done in the global namespace
@@ -889,11 +886,11 @@ void setClipboard(const QString &str) {
 }
 
 fs::path qstringToBoostPath(const QString &path) {
-    return fs::path(path.toStdString(), utf8);
+    return fs::path(path.toStdString());
 }
 
 QString boostPathToQString(const fs::path &path) {
-    return QString::fromStdString(path.string(utf8));
+    return QString::fromStdString(path.string());
 }
 
 QString formatDurationStr(int secs) {

commit 1f1f9bfcf92862cbfff0a0138e7286a117ab7ffd

utils: run commands using utf-8 string on Windows
Summary:
```
Use unicode string to call commans
```

Backport of core PR13886
https://github.com/bitcoin/bitcoin/pull/13886/files

Will fix gitian windows build issue introduced in D3241.

Test Plan:
  make check
Build for windows:
```
mkdir buildcmake && cd buildcmake
cmake -GNinja .. -DCMAKE_TOOLCHAIN_FILE=../cmake/platforms/Win64.cmake \
-DBUILD_BITCOIN_SEEDER=OFF
ninja
```

Reviewers: #bitcoin_abc, deadalnix, jasonbcox

Reviewed By: #bitcoin_abc, jasonbcox

Differential Revision: https://reviews.bitcoinabc.org/D3272

src/util.cpp
@@ -70,6 +70,7 @@
 #ifndef NOMINMAX
 #define NOMINMAX
 #endif
+#include <codecvt>
 
 #include <io.h> /* for _commit */
 #include <shlobj.h>
@@ -1086,7 +1087,14 @@ void runCommand(const std::string &strCommand) {
     if (strCommand.empty()) {
         return;
     }
+#ifndef WIN32
     int nErr = ::system(strCommand.c_str());
+#else
+    int nErr = ::_wsystem(
+        std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>, wchar_t>()
+            .from_bytes(strCommand)
+            .c_str());
+#endif
     if (nErr) {
         LogPrintf("runCommand error: system(%s) returned %d\n", strCommand,
                   nErr);