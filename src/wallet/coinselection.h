ps1dr3x/bitcoin

Remove coinselection.h -> wallet.h circular dependency
Changes CInputCoin to coinselection and to use CTransactionRef in
order to avoid a circular dependency. Also moves other coin selection
specific variables out of wallet.h to coinselectoin.h

src/wallet/coinselection.h
@@ -8,7 +8,44 @@
 #include <amount.h>
 #include <primitives/transaction.h>
 #include <random.h>
-#include <wallet/wallet.h>
+
+//! target minimum change amount
+static const CAmount MIN_CHANGE = CENT;
+//! final minimum change amount after paying for fees
+static const CAmount MIN_FINAL_CHANGE = MIN_CHANGE/2;
+
+class CInputCoin {
+public:
+    CInputCoin(const CTransactionRef& tx, unsigned int i)
+    {
+        if (!tx)
+            throw std::invalid_argument("tx should not be null");
+        if (i >= tx->vout.size())
+            throw std::out_of_range("The output index is out of range");
+
+        outpoint = COutPoint(tx->GetHash(), i);
+        txout = tx->vout[i];
+        effective_value = txout.nValue;
+    }
+
+    COutPoint outpoint;
+    CTxOut txout;
+    CAmount effective_value;
+    CAmount fee = 0;
+    CAmount long_term_fee = 0;
+
+    bool operator<(const CInputCoin& rhs) const {
+        return outpoint < rhs.outpoint;
+    }
+
+    bool operator!=(const CInputCoin& rhs) const {
+        return outpoint != rhs.outpoint;
+    }
+
+    bool operator==(const CInputCoin& rhs) const {
+        return outpoint == rhs.outpoint;
+    }
+};
 
 bool SelectCoinsBnB(std::vector<CInputCoin>& utxo_pool, const CAmount& target_value, const CAmount& cost_of_change, std::set<CInputCoin>& out_set, CAmount& value_ret, CAmount not_input_fees);
 

bitfinitechain/bitfinite-core

Summary:
This is a partial backport of Core PR10637 : https://github.com/bitcoin/bitcoin/pull/10637/commits/4b2716da46e96c45206db869b83c28c5fc7889f4
Depends on D3245 and D3249
Test Plan:
  make check
Reviewers: #bitcoin_abc, Fabien
Reviewed By: #bitcoin_abc, Fabien
Differential Revision: https://reviews.bitcoinabc.org/D3250

src/wallet/coinselection.h
@@ -8,7 +8,45 @@
 #include <amount.h>
 #include <primitives/transaction.h>
 #include <random.h>
-#include <wallet/wallet.h>
+
+//! target minimum change amount
+static const Amount MIN_CHANGE = CENT;
+//! final minimum change amount after paying for fees
+static const Amount MIN_FINAL_CHANGE = MIN_CHANGE / 2;
+
+class CInputCoin {
+public:
+    CInputCoin(const CTransactionRef &tx, unsigned int i) {
+        if (!tx) {
+            throw std::invalid_argument("tx should not be null");
+        }
+        if (i >= tx->vout.size()) {
+            throw std::out_of_range("The output index is out of range");
+        }
+
+        outpoint = COutPoint(tx->GetId(), i);
+        txout = tx->vout[i];
+        effective_value = txout.nValue;
+    }
+
+    COutPoint outpoint;
+    CTxOut txout;
+    Amount effective_value;
+    Amount fee = Amount::zero();
+    Amount long_term_fee = Amount::zero();
+
+    bool operator<(const CInputCoin &rhs) const {
+        return outpoint < rhs.outpoint;
+    }
+
+    bool operator!=(const CInputCoin &rhs) const {
+        return outpoint != rhs.outpoint;
+    }
+
+    bool operator==(const CInputCoin &rhs) const {
+        return outpoint == rhs.outpoint;
+    }
+};
 
 bool SelectCoinsBnB(std::vector<CInputCoin> &utxo_pool,
                     const Amount &target_value, const Amount &cost_of_change,