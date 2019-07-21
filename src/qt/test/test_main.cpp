bitcoincore-dev/bitcoin-2001-863168-294116-d812cf1189

[tests] [qt] Add tests for address book manipulation via EditAddressDialog
Also modifies corresponding QT code to allow for use within test cases.

src/qt/test/test_main.cpp
@@ -13,6 +13,7 @@
 #include <qt/test/compattests.h>
 
 #ifdef ENABLE_WALLET
+#include <qt/test/addressbooktests.h>
 #include <qt/test/paymentservertests.h>
 #include <qt/test/wallettests.h>
 #endif
@@ -97,6 +98,10 @@ int main(int argc, char *argv[])
 #ifdef ENABLE_WALLET
     WalletTests test5;
     if (QTest::qExec(&test5) != 0) {
+        fInvalid = true;
+    }
+    AddressBookTests test6;
+    if (QTest::qExec(&test6) != 0) {
         fInvalid = true;
     }
 #endif

bitfinitechain/bitfinite-core

Summary:
This is a partial backport of Core PR12830 : https://github.com/bitcoin/bitcoin/pull/12830/commits/5109fc4a9cb2cbd73c33197fb9129e1413ab051b
Depends on D3669, D3666 and D3665
Test Plan:
  make check
  ./test/functional/test_runner.py
Reviewers: #bitcoin_abc, jasonbcox
Reviewed By: #bitcoin_abc, jasonbcox
Subscribers: jasonbcox
Differential Revision: https://reviews.bitcoinabc.org/D3670

src/qt/test/test_main.cpp
@@ -17,6 +17,7 @@
 #include <qt/test/rpcnestedtests.h>
 #include <qt/test/uritests.h>
 #ifdef ENABLE_WALLET
+#include <qt/test/addressbooktests.h>
 #include <qt/test/paymentservertests.h>
 #include <qt/test/wallettests.h>
 #endif
@@ -102,6 +103,10 @@ int main(int argc, char *argv[]) {
 #ifdef ENABLE_WALLET
     WalletTests test7;
     if (QTest::qExec(&test7) != 0) {
+        fInvalid = true;
+    }
+    AddressBookTests test8;
+    if (QTest::qExec(&test8) != 0) {
         fInvalid = true;
     }
 #endif