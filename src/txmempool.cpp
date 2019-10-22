equalishcoin/equalishcoind

mempool: remove unused magic number from consistency check

src/txmempool.cpp
@@ -601,7 +601,7 @@ static void CheckInputsAndUpdateCoins(const CTransaction& tx, CCoinsViewCache& m
     CAmount txfee = 0;
     bool fCheckResult = tx.IsCoinBase() || Consensus::CheckTxInputs(tx, state, mempoolDuplicate, spendheight, txfee);
     assert(fCheckResult);
-    UpdateCoins(tx, mempoolDuplicate, 1000000);
+    UpdateCoins(tx, mempoolDuplicate, std::numeric_limits<int>::max());
 }
 
 void CTxMemPool::check(const CCoinsViewCache *pcoins) const

bitfinitechain/bitfinite-core

Summary:
Unexplained magic numbers are no good. Since the exact number does not matter, opt for a constant that is less peculiar.
Backport of Bitcoin Core PR16056
https://github.com/bitcoin/bitcoin/pull/16056
Test Plan:
```
ninja check
Reviewers: Fabien, #bitcoin_abc, deadalnix
Reviewed By: #bitcoin_abc, deadalnix
Differential Revision: https://reviews.bitcoinabc.org/D4278

src/txmempool.cpp
@@ -686,7 +686,7 @@ static void CheckInputsAndUpdateCoins(const CTransaction &tx,
         tx.IsCoinBase() || Consensus::CheckTxInputs(tx, state, mempoolDuplicate,
                                                     spendheight, txfee);
     assert(fCheckResult);
-    UpdateCoins(mempoolDuplicate, tx, 1000000);
+    UpdateCoins(mempoolDuplicate, tx, std::numeric_limits<int>::max());
 }
 
 void CTxMemPool::check(const CCoinsViewCache *pcoins) const {