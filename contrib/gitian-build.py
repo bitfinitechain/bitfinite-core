Bitcoin-ABC/bitcoin-abc

build: macOS toolchain update
Summary:
```
TLDR: This updates our macOS toolchain to use a newer version of Clang,
cctools (including new dependency on libtapi), LD64 and the macOS SDK.

I've been testing depends builds (HOST=x86_64-apple-darwin16) inside a
Debian Buster Docker container, and running the resultant bitcoind and
bitcoin-qt binaries on a macOS 10.14.4 system. The .dmg generated by a
make deploy also mounts correctly on the same macOS system.
Clang

Upgraded from 3.7.1 to 6.0.1
cctools

    cctools 877.8 -> 921
    LD64 253.9 -> 409.12
    TAPI 1000.10.8

See tpoechtrager/cctools-port and tpoechtrager/apple-libtapi.
macOS SDK

Upgraded from building against the macOS 10.11 SDK to the macOS 10.14
SDK.
```

Backport of core [[https://github.com/bitcoin/bitcoin/pull/16392 | PR16392]].

Depends on D5530.

Test Plan:
Run the OSX Gitian build twice and check it is deterministic.
Run the app on OSX (10.12 and 10.15) and check it runs.

Reviewers: #bitcoin_abc, deadalnix

Reviewed By: #bitcoin_abc, deadalnix

Subscribers: deadalnix

Differential Revision: https://reviews.bitcoinabc.org/D5531

contrib/gitian-build.py
@@ -262,7 +262,7 @@ def main():
 
     # Disable for MacOS if no SDK found
     if args.macos and not os.path.isfile(
-            'gitian-builder/inputs/MacOSX10.11.sdk.tar.gz'):
+            'gitian-builder/inputs/MacOSX10.14.sdk.tar.gz'):
         print('Cannot build for MacOS, SDK does not exist. Will build for other OSes')
         args.macos = False
 

bitfinitechain/bitfinite-core

contrib/gitian-build.py
@@ -262,7 +262,7 @@ def main():
 
     # Disable for MacOS if no SDK found
     if args.macos and not os.path.isfile(
-            'gitian-builder/inputs/MacOSX10.11.sdk.tar.xz'):
+            'gitian-builder/inputs/MacOSX10.14.sdk.tar.xz'):
         print('Cannot build for MacOS, SDK does not exist. Will build for other OSes')
         args.macos = False
 