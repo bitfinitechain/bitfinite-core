Bitcoin-ABC/bitcoin-abc

Pass rpc/abc RPC Results and Examples to RPCHelpMan
Summary: Partial backport of Core [[https://github.com/bitcoin/bitcoin/pull/14987 | PR14987]]

Test Plan:
On master:
  ninja
  ./bitcoind
  ./bitcoin-cli help > RPCs
  ./getrpchelps.sh RPCs master
Then on the patched branch:
  ninja
  ./bitcoind
  ./getrpchelps.sh RPCs abcPatched
  ./checkrpc.sh RPCs master abcPatched abc.diff
Verify the changes in `abc.diff`.

Scripts:
{F4258185}
{F4258184}

abc.diff:
{F4258186}

Reviewers: O1 Bitcoin ABC, #bitcoin_abc, Fabien

Reviewed By: O1 Bitcoin ABC, #bitcoin_abc, Fabien

Subscribers: Fabien

Differential Revision: https://reviews.bitcoinabc.org/D5809

src/rpc/abc.cpp
@@ -14,15 +14,15 @@
 static UniValue getexcessiveblock(const Config &config,
                                   const JSONRPCRequest &request) {
     if (request.fHelp || request.params.size() != 0) {
-        throw std::runtime_error(
-            RPCHelpMan{
-                "getexcessiveblock", "\nReturn the excessive block size.", {}}
-                .ToString() +
-            "\nResult\n"
-            "  excessiveBlockSize (integer) block size in bytes\n"
-            "\nExamples:\n" +
-            HelpExampleCli("getexcessiveblock", "") +
-            HelpExampleRpc("getexcessiveblock", ""));
+        throw std::runtime_error(RPCHelpMan{
+            "getexcessiveblock",
+            "\nReturn the excessive block size.",
+            {},
+            RPCResult{"  excessiveBlockSize (integer) block size in bytes\n"},
+            RPCExamples{HelpExampleCli("getexcessiveblock", "") +
+                        HelpExampleRpc("getexcessiveblock", "")},
+        }
+                                     .ToStringWithResultsAndExamples());
     }
 
     UniValue ret(UniValue::VOBJ);
@@ -33,24 +33,22 @@ static UniValue getexcessiveblock(const Config &config,
 static UniValue setexcessiveblock(Config &config,
                                   const JSONRPCRequest &request) {
     if (request.fHelp || request.params.size() != 1) {
-        throw std::runtime_error(
-            RPCHelpMan{
-                "setexcessiveblock",
-                "\nSet the excessive block size. Excessive blocks will not be "
-                "used in the active chain or relayed. This discourages the "
-                "propagation of blocks that you consider excessively large.",
-                {
-                    {"blockSize", RPCArg::Type::NUM, /* opt */ false,
-                     /* default_value */ "",
-                     "Excessive block size in bytes.  Must be greater than " +
-                         std::to_string(LEGACY_MAX_BLOCK_SIZE) + "."},
-                }}
-                .ToString() +
-            "\nResult\n"
-            "  blockSize (integer) excessive block size in bytes\n"
-            "\nExamples:\n" +
-            HelpExampleCli("setexcessiveblock", "25000000") +
-            HelpExampleRpc("setexcessiveblock", "25000000"));
+        throw std::runtime_error(RPCHelpMan{
+            "setexcessiveblock",
+            "\nSet the excessive block size. Excessive blocks will not be used "
+            "in the active chain or relayed. This discourages the propagation "
+            "of blocks that you consider excessively large.",
+            {
+                {"blockSize", RPCArg::Type::NUM, /* opt */ false,
+                 /* default_value */ "",
+                 "Excessive block size in bytes.  Must be greater than " +
+                     std::to_string(LEGACY_MAX_BLOCK_SIZE) + "."},
+            },
+            RPCResult{"  blockSize (integer) excessive block size in bytes\n"},
+            RPCExamples{HelpExampleCli("setexcessiveblock", "25000000") +
+                        HelpExampleRpc("setexcessiveblock", "25000000")},
+        }
+                                     .ToStringWithResultsAndExamples());
     }
 
     if (!request.params[0].isNum()) {

bitfinitechain/bitfinite-core

Backport of Bitcoin ABC [D5809](https://reviews.bitcoinabc.org/D5809)
(partial backport of Bitcoin Core
[PR#14987](https://github.com/bitcoin/bitcoin/pull/14987))
Replaced some copies with moves compared to upstream. Also fixed a bug
in the return value documentation of setexcessiveblock.
Test plan:
* `bitcoin-cli help getexcessiveblock`
* `bitcoin-cli help setexcessiveblock`
* `bitcoin-cli setexcessiveblock 32000000`
* `bitcoin-cli help getblockchaininfo`

src/rpc/abc.cpp
@@ -5,6 +5,7 @@
 #include <config.h>
 #include <consensus/consensus.h>
 #include <rpc/server.h>
+#include <rpc/util.h>
 #include <util/strencodings.h>
 #include <validation.h>
 
@@ -13,14 +14,14 @@
 static UniValue getexcessiveblock(const Config &config,
                                   const JSONRPCRequest &request) {
     if (request.fHelp || request.params.size() != 0) {
-        throw std::runtime_error(
-            "getexcessiveblock\n"
-            "\nReturn the excessive block size."
-            "\nResult\n"
-            "  excessiveBlockSize (integer) block size in bytes\n"
-            "\nExamples:\n" +
-            HelpExampleCli("getexcessiveblock", "") +
-            HelpExampleRpc("getexcessiveblock", ""));
+        throw std::runtime_error(RPCHelpMan{
+            "getexcessiveblock",
+            "\nReturn the excessive block size.",
+            {},
+            RPCResult{"  excessiveBlockSize (integer) block size in bytes\n"},
+            RPCExamples{HelpExampleCli("getexcessiveblock", "") +
+                        HelpExampleRpc("getexcessiveblock", "")},
+        }.ToStringWithResultsAndExamples());
     }
 
     UniValue::Object ret;
@@ -32,21 +33,21 @@ static UniValue getexcessiveblock(const Config &config,
 static UniValue setexcessiveblock(Config &config,
                                   const JSONRPCRequest &request) {
     if (request.fHelp || request.params.size() != 1) {
-        throw std::runtime_error(
-            "setexcessiveblock maxBlockSize\n"
+        throw std::runtime_error(RPCHelpMan{
+            "setexcessiveblock",
             "\nSet the excessive block size. Excessive blocks will not be used "
             "in the active chain or relayed. This discourages the propagation "
-            "of blocks that you consider excessively large."
-            "\nArguments\n"
-            "1. maxBlockSize  (integer, required) Excessive block size in bytes.  "
-            "Must be greater than " +
-            std::to_string(LEGACY_MAX_BLOCK_SIZE) +
-            ".\n"
-            "\nResult\n"
-            "  maxBlockSize (integer) excessive block size in bytes\n"
-            "\nExamples:\n" +
-            HelpExampleCli("setexcessiveblock", "128000000") +
-            HelpExampleRpc("setexcessiveblock", "128000000"));
+            "of blocks that you consider excessively large.",
+            {
+                {"maxBlockSize", RPCArg::Type::NUM, /* opt */ false,
+                 /* default_value */ "",
+                 "Excessive block size in bytes. Must be greater than " +
+                     std::to_string(LEGACY_MAX_BLOCK_SIZE) + "."},
+            },
+            RPCResult{"\"Status info.\" (string) human-readable result\n"},
+            RPCExamples{HelpExampleCli("setexcessiveblock", "128000000") +
+                        HelpExampleRpc("setexcessiveblock", "128000000")},
+        }.ToStringWithResultsAndExamples());
     }
 
     if (!request.params[0].isNum()) {